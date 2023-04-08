1d06d805bbfe0c27b7af38189a132b8b4ba2de62
Sarthak-Dayal <63827830+Sarthak-Dayal@users.noreply.github.com>
2023-04-08 14:50:33 -0500

    set up initial front end

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,46 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+/** @format */
+
+import Head from "next/head";
+import { useState } from "react";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
-    <div className="root">
+    <div className='root'>
       <Head>
         <title>GPT-3 Writer | buildspace</title>
       </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+      <div className='container'>
+        <div className='header'>
+          <div className='header-title'>
+            <h1>uil study tool</h1>
           </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+          <div className='header-subtitle'>
+            <h2>
+              input a uil question below, we'll give you all the surrounding
+              information you need to learn
+            </h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className='prompt-container'>
+          <textarea
+            placeholder='high school or middle school uil question (answer choices optional)'
+            className='prompt-box'
+            value={input}
+            onChange={onInputChange}
+          />
+          <div className='prompt-buttons'>
+            <a className='generate-button' onClick={null}>
+              <div className='generate'>
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
