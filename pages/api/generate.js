67e79102eb13dd2842b75d1bcc6a4d7d5cb5d4b9
Sarthak-Dayal <63827830+Sarthak-Dayal@users.noreply.github.com>
2023-04-09 13:43:44 -0500

    fine-tune model and update character list

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,23 +9,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function generateAction(req, res) {
-  const voices = [
-    "pirate",
-    "donald duck",
-    "goofy (character)",
-    "batman",
-    "boss baby",
-  ];
-  const basePromptPrefix = `Explain how I should answer this question, explain concepts for all answer choices when applicable, and tell me the concepts I should study further with specific resource links for an advanced high school student in a ${
+  const voices = ["pirate", "donald duck", "Goofy", "batman", "boss baby"];
+  const basePromptPrefix = `Explain how I should answer this question, explain concepts for all answer choices when applicable, and tell me the concepts I should study further with specific resource links for an advanced high school student in language that resembles the way ${
     voices[Math.floor(Math.random() * voices.length)]
-  } voice: `;
+  } speaks: `;
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.8,
+    temperature: 0.6,
     max_tokens: 1000,
   });
 
